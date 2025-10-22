980cd43c865e92684a75e3b9708ed61c2465ad15
Egor M <egor.mishchuk17@gmail.com>
2025-10-22 21:56:34 +0700

    full create internal config file

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,14 @@
 package app
+
+import (
+	"context"
+	"tusur-forms/internal/config"
+)
+
+func Run() {
+	ctx := context.Background()
+	cfg, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	RedirectUrl  string
+}
+
+func NewConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+	config := &Config{
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		RedirectUrl:  os.Getenv("REDIRECT_URL"),
+	}
+	return config, nil
+}
