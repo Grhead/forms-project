d4efdb23156292906a5629025fb4da88e804cfb3
Egor M <egor.mishchuk17@gmail.com>
2025-11-02 22:24:04 +0700

    Update configs getting. Update env examples

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/forms/v1"
@@ -29,8 +30,12 @@ type DatabaseProvider interface {
 type EnvConfigProvider struct{}
 type DbSQLiteProvider struct{}
 
-func (e *EnvConfigProvider) LoadFormConfig() (FormConfig, error) {
-	cfg := FormConfig{
+func (e *EnvConfigProvider) LoadFormConfig() (*FormConfig, error) {
+	err := godotenv.Load("configs/.env.form")
+	if err != nil {
+		return nil, err
+	}
+	cfg := &FormConfig{
 		clientID:     os.Getenv("CLIENT_ID"),
 		clientSecret: os.Getenv("CLIENT_SECRET"),
 		redirectUrl:  os.Getenv("REDIRECT_URL"),
@@ -38,18 +43,22 @@ func (e *EnvConfigProvider) LoadFormConfig() (FormConfig, error) {
 	return cfg, nil
 }
 
-func (e *EnvConfigProvider) LoadDBConfig() (DbConfig, error) {
-	cfg := DbConfig{
+func (e *EnvConfigProvider) LoadDBConfig() (*DbConfig, error) {
+	err := godotenv.Load("configs/.env.database")
+	if err != nil {
+		return nil, err
+	}
+	cfg := &DbConfig{
 		fileName: os.Getenv("FILE_NAME"),
 	}
 	return cfg, nil
 }
 
-func (s *DbSQLiteProvider) Connect(cfg DbConfig) (*gorm.DB, error) {
+func (s *DbSQLiteProvider) Connect(cfg *DbConfig) (*gorm.DB, error) {
 	return gorm.Open(sqlite.Open(cfg.fileName), &gorm.Config{})
 }
 
-func NewOAuth2Config(cfg FormConfig) *oauth2.Config {
+func NewOAuth2Config(cfg *FormConfig) *oauth2.Config {
 	return &oauth2.Config{
 		ClientID:     cfg.clientID,
 		ClientSecret: cfg.clientSecret,
